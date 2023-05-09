a99cd07be180e7a398bee2144fb6f0820c438138
WalaaAlrefai <lulusedqi@gmail.com>
2023-05-09 07:55:37 +0000

    its work

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,6 +1,6 @@
 import re
 
-file = "assets/madlib.txt"
+file = "/home/student/madlib-cli/assets/dark_and_stormy_night.txt"
 
 def opening():
     print("""
@@ -24,8 +24,35 @@ def read_template(template):
 
 
 
+def parse_template(template):
+    '''
+    This is the parse_template
+    it will take a tuple as a input
+    return the tuple formated and the words seperated
+    parse_template("It was a {Adjective} and {Adjective} {Noun}.") = 'It was a {} and {} {}' and ('dark', 'stormy', 'night')'''
+    expected_stripped = r"{([\w ',.-]+)}"
+    expected_parts_list = tuple(re.findall(expected_stripped, template))
+    expected_parts = re.sub(expected_stripped, '{}', template)
+    return expected_parts, expected_parts_list
+
+
+def merge(string, words):
+    '''
+    This is the merge function
+    it will take a string and a tuple with 3 options
+    merge("It was a {} and {} {}.", ("dark", "stormy", "night") = 'It was a dark and story night'
+    '''
+    return string.format(*words)
+
+
+
 if __name__ == "__main__":
     opening()
-    template = read_template("assets/madlib.txt")
+    template = read_template(file)
+    expected_parts, expected_parts_list = parse_template(template)
+    word_text = word_list(expected_parts_list)
+    mad_lib = merge(expected_parts, word_text)
+    print(mad_lib)
+    print('Finished your Madlib!')
 
 
