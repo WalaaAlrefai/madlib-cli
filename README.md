b49ab7abd49b9131e1b7546e018a6e9566dfba06
WalaaAlrefai <lulusedqi@gmail.com>
2023-05-09 01:12:47 +0000

    not completed

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-LAB - Class 03
-Project: madlib_cli
-Author: Walaa Alrefai
+## LAB - Class 03
+## Project: madlib_cli
+### Author: Walaa Alrefai
 
 Setup
 .env requirements (where applicable)
 i.e.
+       exceptiongroup==1.1.1
+       iniconfig==2.0.0
+       packaging==23.1
+       pluggy==1.0.0
+       pytest==7.3.1
+       tomli==2.0.1
 
-PORT - Port Number
-DATABASE_URL - URL to the running Postgres instance/db
+
+
+
 How to initialize/run your application (where applicable)
-e.g. python main.py
-How to use your library (where applicable)
+e.g. python madlib.txt
+
 Tests
 How do you run tests?
+pytest
 Any tests of note?
-Describe any tests that you did not complete, skipped, etc
+my file doesnt opened , so I cant complete work
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,5 @@
 import re
-file = "../assets/madlibTemp.txt"
+file = "./assets/madlib.txt"
 def opening():
     print("""
   **************************************
@@ -20,37 +20,10 @@ def read_template(template):
         read_text = read_file.read()
         return read_text
 
-def parse_template(template):
-    '''
-    This is the parse_template
-    it will take a tuple as a input
-    with that file it will format out the words Adjective and Noun to {}
-    then it will go through that same tuple and find all the words that were taken out and return a list
-    it will then convert that new list into a tuple
-    it will then return the tuple formated and the words seperated
-    parse_template("It was a {Adjective} and {Adjective} {Noun}.") = 'It was a {} and {} {}' and ('dark', 'stormy', 'night')'''
-    expected_stripped = r"{([\w ',.-]+)}"
-    expected_parts_list = tuple(re.findall(expected_stripped, template))
-    expected_parts = re.sub(expected_stripped, '{}', template)
-    return expected_parts, expected_parts_list
 
-def merge(string, words):
-    '''
-    This is the merge function
-    it will take a string and a tuple with 3 options
-    with that it will format the string to now include the options within the tuple
-    then it will return that newly formed string
-    merge("It was a {} and {} {}.", ("dark", "stormy", "night") = 'It was a dark and story night'
-    '''
-    return string.format(*words)   
 
 if __name__ == "__main__":
     opening()
     template = read_template(file)
-    expected_parts, expected_parts_list = parse_template(template)
-    word_text = word_list(expected_parts_list)
-    mad_lib = merge(expected_parts, word_text)
-    print(mad_lib)
-    print('Finished your Madlib!')
 
 
